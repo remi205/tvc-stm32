c45da9465af459f39b5534bd6623384cd3310ff2
remi prud'homme <prudhomme.remi@gmail.com>
2015-06-13 15:55:35 +0200

    console : ++

--- over-hal/Console.c
+++ over-hal/Console.c
@@ -2,34 +2,57 @@
 #include <stm32f4xx_hal_usart.h>
 #include <string.h>
 
-extern struct UART_HandleTypeDef huart5;
-extern struct UART_HandleTypeDef huart2;
-extern struct UART_HandleTypeDef huart6;
+
+#ifdef uart1
 extern struct UART_HandleTypeDef huart1;
+#endif
+
+#ifdef uart2
 extern struct UART_HandleTypeDef huart2;
+#endif
+
+#ifdef uart3
 extern struct UART_HandleTypeDef huart3;
+#endif
+
+#ifdef uart4
+extern struct UART_HandleTypeDef huart4;
+#endif
+
+#ifdef uart5
+extern struct UART_HandleTypeDef huart5;
+#endif
+
+#ifdef uart6
+extern struct UART_HandleTypeDef huart6;
+#endif
 
 struct UART_HandleTypeDef * Handle;
 
 struct UART_HandleTypeDef * OpenConsole( char * name)
 {
 #ifdef uart6
-  Handle =(struct UART_HandleTypeDef * )&huart6;
+  Handle =(struct UART_HandleTypeDef *)&huart6;
 #endif
+
 #ifdef uart5
-  Handle = &huart5;
+  Handle = (struct UART_HandleTypeDef *)&huart5;
 #endif
+
 #ifdef uart4
-  Handle = &huart4;
+  Handle = (struct UART_HandleTypeDef * )&huart4;
 #endif
+
 #ifdef uart3
-  Handle = &huart3;
+  Handle = (struct UART_HandleTypeDef * )&huart3;
 #endif
+
 #ifdef uart2
-  Handle = &huart2;
+  Handle = (struct UART_HandleTypeDef * )&huart2;
 #endif
+
 #ifdef uart1
-  Handle = &huart1;
+  Handle = (struct UART_HandleTypeDef *)&huart1;
 #endif
   return Handle; 
 }
